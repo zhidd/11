a07592b54b7ab3d8f38efd3d61c175e1dcae9a13
Your Name <author@corpmail.com>
2020-11-01 23:26:23 +0800

    dd

---
+++ huux_tinymce/install.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+    Xiuno BBS 4.0 大白插件
+    http://www.huux.cc
+ */
+
+!defined('DEBUG') AND exit('Forbidden');
+
+// 同类插件互相禁用，禁用百度编辑器
+plugin_unstall('xn_umeditor');
+
+?>
---
+++ huux_tinymce/unstall.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+    Xiuno BBS 4.0 大白插件
+    http://www.huux.cc
+ */
+
+!defined('DEBUG') AND exit('Forbidden');
+
+
+
+?>
